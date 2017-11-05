fd2dba06c436d1dd29d135a14cb8961e169d722f
kaushik-l <jklakshm@bcm.edu>
2017-11-04 21:43:19 -0500

    accept binrange instead of binedges

--- Encode1hot.m
+++ Encode1hot.m
@@ -1,6 +1,7 @@
-function [x_1hot,xvals] = Encode1hot(xt,binedges)
+function [x_1hot,xvals,nprs] = Encode1hot(xt,binrange,nbins)
 
 % initialise with zeros
+binedges = linspace(binrange(1),binrange(2),nbins+1);
 xvals = 0.5*(binedges(1:end-1) + binedges(2:end));
 x_1hot = zeros(length(xt),length(xvals));
 
@@ -9,3 +10,4 @@ for i = 1:numel(xt)
     [~, indx] = min(abs(xt(i)-xvals));
     x_1hot(i,indx) = 1;
 end
+nprs = size(x_1hot,2);
